48397b6b485af1b688371f04ab5a28f6fb785a5c
Harshniket Seta <harshniketseta@gmail.com>
2015-05-31 12:19:45 +0530

    Updated dist, enable and disable code added.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+
+  $("input.enable").on("click", function(){
+    $("#industries2").multiselect("enable");
+  });
+
+  $("input.disable").on("click", function(){
+    $("#industries2").multiselect("disable");
+  });
+
   $("#industries1").multiselect({title: "Select Industry", maxSelectionAllowed: 5});
   $("#industries2").multiselect({title: "Select Industry", maxSelectionAllowed: 5});
 });
