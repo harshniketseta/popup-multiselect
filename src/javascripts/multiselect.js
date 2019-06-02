9e6bed8ba159f6cf8befdb6961ee06fafea82e2f
Harshniket Seta <harshniketseta@gmail.com>
2019-06-03 04:02:37 +0530

    update to versionn 2.1.3 with updated license information.

--- src/javascripts/multiselect.js
+++ src/javascripts/multiselect.js
@@ -4,7 +4,7 @@
  * Popup Multi Select
  * Created By Harshniket Seta
  * Currently maintained by harshniketseta [at] gmail [dot] com
- * Version: 2.1.2
+ * Version: 2.1.3
  * Release: 2019-06-03
  * ======================================================================== */
 
